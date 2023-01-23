7a7b2f4dc8158ce47bc445839c1c7963f3b069be
Cristian <ctomatfl@gmail.com>
2023-01-23 10:22:13 -0400

    Create AppProfile molecule

---
+++ src/components/molecules/AppProfile/AppProfile.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import AppAvatar from "../../atoms/AppAvatar/AppAvatar.svelte";
+    import AppText from "../../atoms/AppTitle/AppText.svelte";
+    import BellRingOutline from "../../../assets/svg/BellRingOutline.svelte";
+
+    export let avatarImage: string
+    export let avatarSize: 'Small' | 'Medium' | 'Big'
+    export let username: string
+    export let color: 'primary' | 'secondary'
+</script>
+
+<div class="profile-container">
+    <figure>
+        <BellRingOutline />
+    </figure>
+    <AppAvatar {avatarImage} {avatarSize} />
+    <AppText text={username} textType='h4' {color}/>
+</div>
+
+<style>
+    .profile-container {
+        display: flex;
+        align-items: center;
+        gap: 40px;
+    }
+
+    .profile-container figure {
+        width: 30px;
+        height: auto;
+        fill: var(--text-primary-color);
+        cursor: pointer;
+    }
+
+    .profile-container figure:hover {
+        width: 30px;
+        height: auto;
+        fill: var(--text-secondary-color);
+    }
+</style>
