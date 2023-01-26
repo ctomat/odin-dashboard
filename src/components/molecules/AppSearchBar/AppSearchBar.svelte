80b3fdb06f5e798e7058ee3f45f364520a8f8a23
Cristian <ctomatfl@gmail.com>
2023-01-26 11:01:12 -0400

    Create searchbar for navigation

---
+++ src/components/molecules/AppSearchBar/AppSearchBar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import AppInput from "../../atoms/AppInput/AppInput.svelte";
+
+    import CardSearchOutline from "../../../assets/svg/CardSearchOutline.svelte";
+    
+    export let value: string = '' 
+</script>
+
+<div class="searchbar-container">
+    <figure>
+        <CardSearchOutline />
+    </figure>
+    <div class="input-container">
+        <AppInput {value} on:input={() => {}} />
+    </div>
+</div>
+
+<style>
+    .searchbar-container {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+    .input-container {
+        width: 100%;
+    }
+    figure {
+        width: 40px;
+        height: auto;
+        fill: var(--text-primary-color);
+        cursor: pointer;
+    }
+</style>
