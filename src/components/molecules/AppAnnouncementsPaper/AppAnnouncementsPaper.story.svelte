be193750fa83177828953568d2839cb686ffcd1f
Cristian <ctomatfl@gmail.com>
2023-01-30 13:27:14 -0400

    Create AppAnnouncementsPaper molecule

---
+++ src/components/molecules/AppAnnouncementsPaper/AppAnnouncementsPaper.story.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import type { Hst } from "@histoire/plugin-svelte";
+
+  import AppAnnouncementsPaper from "./AppAnnouncementsPaper.svelte";
+
+  export let Hst: Hst;
+</script>
+
+<Hst.Story title="Components/Molecules/AppAnnouncementsPaper">
+  <Hst.Variant title="Default">
+    <AppAnnouncementsPaper
+      announcements={[
+        {
+          title: "Ad 1",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus placerat odio ac tempor. Pellentesque vehicula tortor orci, vel egestas lorem tincidunt vitae. Etiam odio nunc, maximus ut odio non, finibus consequat urna. Nullam eu turpis vel libero feugiat euismod vitae sit amet ex. Nulla sollicitudin pretium odio quis eleifend. Cras ac tempor libero.",
+        },
+        {
+          title: "Ad 2",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus placerat odio ac tempor. Pellentesque vehicula tortor orci, vel egestas lorem tincidunt vitae. Etiam odio nunc, maximus ut odio non, finibus consequat urna. Nullam eu turpis vel libero feugiat euismod vitae sit amet ex. Nulla sollicitudin pretium odio quis eleifend. Cras ac tempor libero.",
+        },
+        {
+          title: "Ad 3",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus placerat odio ac tempor. Pellentesque vehicula tortor orci, vel egestas lorem tincidunt vitae. Etiam odio nunc, maximus ut odio non, finibus consequat urna. Nullam eu turpis vel libero feugiat euismod vitae sit amet ex. Nulla sollicitudin pretium odio quis eleifend. Cras ac tempor libero.",
+        },
+        {
+          title: "Ad 4",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus placerat odio ac tempor. Pellentesque vehicula tortor orci, vel egestas lorem tincidunt vitae. Etiam odio nunc, maximus ut odio non, finibus consequat urna. Nullam eu turpis vel libero feugiat euismod vitae sit amet ex. Nulla sollicitudin pretium odio quis eleifend. Cras ac tempor libero.",
+        },
+      ]}
+    />
+  </Hst.Variant>
+  <Hst.Variant title="Truncate">
+    <AppAnnouncementsPaper
+      numberOfCharactersToTruncate={90}
+      announcements={[
+        {
+          title: "Ad 1",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus placerat odio ac tempor. Pellentesque vehicula tortor orci, vel egestas lorem tincidunt vitae. Etiam odio nunc, maximus ut odio non, finibus consequat urna. Nullam eu turpis vel libero feugiat euismod vitae sit amet ex. Nulla sollicitudin pretium odio quis eleifend. Cras ac tempor libero.",
+        },
+        {
+          title: "Ad 2",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus placerat odio ac tempor. Pellentesque vehicula tortor orci, vel egestas lorem tincidunt vitae. Etiam odio nunc, maximus ut odio non, finibus consequat urna. Nullam eu turpis vel libero feugiat euismod vitae sit amet ex. Nulla sollicitudin pretium odio quis eleifend. Cras ac tempor libero.",
+        },
+        {
+          title: "Ad 3",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus placerat odio ac tempor. Pellentesque vehicula tortor orci, vel egestas lorem tincidunt vitae. Etiam odio nunc, maximus ut odio non, finibus consequat urna. Nullam eu turpis vel libero feugiat euismod vitae sit amet ex. Nulla sollicitudin pretium odio quis eleifend. Cras ac tempor libero.",
+        },
+        {
+          title: "Ad 4",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus placerat odio ac tempor. Pellentesque vehicula tortor orci, vel egestas lorem tincidunt vitae. Etiam odio nunc, maximus ut odio non, finibus consequat urna. Nullam eu turpis vel libero feugiat euismod vitae sit amet ex. Nulla sollicitudin pretium odio quis eleifend. Cras ac tempor libero.",
+        },
+      ]}
+    />
+  </Hst.Variant>
+</Hst.Story>
+
+<style>
+</style>
---
+++ src/components/molecules/AppAnnouncementsPaper/AppAnnouncementsPaper.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import AppPaper from "../../atoms/AppPaper/AppPaper.svelte";
+  import AppText from "../../atoms/AppTitle/AppText.svelte";
+
+  type announcementType = {
+    title: string;
+    description: string;
+  };
+
+  export let announcements: announcementType[] = [];
+  export let numberOfCharactersToTruncate: number = 0;
+
+  function createLastAnnouncements(useWordBoundary: boolean) {
+    let announcementsList: announcementType[] = announcements.filter(
+      (announcement, index) => index < 3
+    );
+
+    if (!numberOfCharactersToTruncate) {
+      return announcementsList;
+    }
+
+    announcementsList = announcementsList.map((announcement) => {
+      const subString = announcement.description.slice(
+        0,
+        numberOfCharactersToTruncate - 1
+      );
+
+      announcement.description =
+        (useWordBoundary
+          ? subString.slice(0, subString.lastIndexOf(" "))
+          : subString) + "...";
+
+      return announcement;
+    });
+
+    return announcementsList;
+  }
+
+  let lastAnnouncements: announcementType[] = createLastAnnouncements(true);
+
+  console.log(lastAnnouncements, "AAAAA");
+</script>
+
+<div class="announcements-container">
+  <header>
+    <AppText text="Announcements" textType="h3" color="primary" />
+  </header>
+  <AppPaper paperStyle="Plain Color">
+    {#each lastAnnouncements as announcement, index}
+      <div class="announcement-container">
+        <AppText text={announcement.title} textType="h4" color="primary" />
+        <AppText
+          text={announcement.description}
+          textType="body"
+          color="primary"
+        />
+      </div>
+      {#if index !== lastAnnouncements.length - 1}
+        <div class="divider" />
+      {/if}
+    {/each}
+  </AppPaper>
+</div>
+
+<style>
+  .announcements-container {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+  .announcement-container {
+    margin: 20px 0;
+  }
+  div.divider {
+    width: 100%;
+    height: 1px;
+    background-color: var(--text-primary-color);
+  }
+</style>
