be390e2e186b9801b89bf6190c47e2ba174534d4
Cristian <ctomatfl@gmail.com>
2023-01-31 19:23:50 -0400

    Add AppTrendingList and AppCardGrid components

---
+++ src/components/molecules/AppTrendingList/AppTrendingList.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import AppPaper from "../../atoms/AppPaper/AppPaper.svelte";
+  import AppTrendingElement from "../../atoms/AppTrendingElement/AppTrendingElement.svelte";
+  import AppText from "../../atoms/AppTitle/AppText.svelte";
+
+  type trendingListType = {
+    userTag: string;
+    associatedProject: string;
+    avatarImage: string;
+  };
+
+  export let trendingList: trendingListType[];
+</script>
+
+<div class="trending-container">
+  <header class="trending-title">
+    <AppText text="Trending" textType="h3" color="primary" />
+  </header>
+  <div class="trending-list">
+    <AppPaper paperStyle="Plain Color">
+      {#each trendingList as trending}
+        <div class="trending">
+          <AppTrendingElement
+            userTag={trending.userTag}
+            associatedProject={trending.associatedProject}
+            avatarImage={trending.avatarImage}
+          />
+        </div>
+      {/each}
+    </AppPaper>
+  </div>
+</div>
+
+<style>
+  .trending {
+    margin-bottom: 30px;
+  }
+  .trending:last-of-type {
+    margin-bottom: 0px;
+  }
+  .trending-title {
+    margin-bottom: 20px;
+  }
+</style>
---
+++ src/components/organism/AppCardGrid/AppCardGrid.story.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import type { Hst } from "@histoire/plugin-svelte";
+
+  import AppCardGrid from "./AppCardGrid.svelte";
+
+  export let Hst: Hst;
+</script>
+
+<Hst.Story title="Components/Organism/AppCardGrid">
+  <Hst.Variant title="Default">
+    <AppCardGrid
+      cardsData={[
+        {
+          cardTitle: "Cool Project Title",
+          cardBody:
+            "Lorem ipusim. pdsadpsa. oeopdspadpsa. epsdsapd  ds saawwdsaas wopdsapa",
+        },
+        {
+          cardTitle: "Cool Project Title",
+          cardBody:
+            "Lorem ipusim. pdsadpsa. oeopdspadpsa. epsdsapd  ds saawwdsaas wopdsapa",
+        },
+        {
+          cardTitle: "Cool Project Title",
+          cardBody:
+            "Lorem ipusim. pdsadpsa. oeopdspadpsa. epsdsapd  ds saawwdsaas wopdsapa",
+        },
+        {
+          cardTitle: "Cool Project Title",
+          cardBody:
+            "Lorem ipusim. pdsadpsa. oeopdspadpsa. epsdsapd  ds saawwdsaas wopdsapa",
+        },
+        {
+          cardTitle: "Cool Project Title",
+          cardBody:
+            "Lorem ipusim. pdsadpsa. oeopdspadpsa. epsdsapd  ds saawwdsaas wopdsapa",
+        },
+        {
+          cardTitle: "Cool Project Title",
+          cardBody:
+            "Lorem ipusim. pdsadpsa. oeopdspadpsa. epsdsapd  ds saawwdsaas wopdsapa",
+        },
+      ]}
+    />
+  </Hst.Variant>
+</Hst.Story>
+
+<style>
+</style>
